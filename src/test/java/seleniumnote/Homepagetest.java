bb9c39efd473c5104dbddd3d28f729b3e5756af3
BHARATHI BATHINA <bharathi.bathina@3i-infotech.com>
2024-03-04 16:00:20 +0530

    Adding Homepagetest file

---
+++ src/test/java/seleniumnote/Homepagetest.java
@@ -0,0 +1,12 @@
+package seleniumnote;
+
+public class Homepagetest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is home page screen");
+
+	}
+
+}
